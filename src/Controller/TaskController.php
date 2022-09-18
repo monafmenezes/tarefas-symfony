7db54b068adbdf9b1bc00356e3852c9ee30a2081
Monalisa Menezes <monalisa.menezes@onesight.com.br>
2022-09-18 14:33:17 -0300

    feat: Ajusta as rotas

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -9,11 +9,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("task")
+ */
 class TaskController extends AbstractController
 {
   /**
    * Lista tarefas do banco de dados
+   * 
+   * @Route("/", name="task_index", methods="GET")
+   * 
+   * 
+   * @return Response
    */
   public function index(ManagerRegistry $doctrine)
   {
@@ -25,30 +34,20 @@ class TaskController extends AbstractController
     ]);
   }
 
- /**
-  * Mostra uma tarefa específica 
-  *
-  * @param Task $task
-  * @param ManagerRegistry $doctrine
-  * @return void
-  */
-
-  public function show(Task $task, ManagerRegistry $doctrine)
-  {
-      return $this->render("tasks/show.html.twig", [
-            "task" => $task
-      ]);
-  }
 
   /**
    * Cria uma nova tarefa
+   * 
+   * @Route("/new", name="task_new", methods={"GET", "POST"})
    *
    * @param Request $request
    * @return Response
    */
   public function new(ManagerRegistry $doctrine, Request $request) : Response
   {
-        if ($request->isMethod("POST")) {
+        $isTokenValid = $this->isCsrfTokenValid('cadastro_tarefas', $request->request->get('_token'));
+
+        if ($request->isMethod("POST") && $isTokenValid) {
 
             // var_dump(
             //   $request->request->get('title')
@@ -80,15 +79,35 @@ class TaskController extends AbstractController
   }
 
   /**
+  * Mostra uma tarefa específica 
+  * 
+  * @Route("/{id}", name="task_show", methods="GET")
+  *
+  * @param Task $task
+  * @param ManagerRegistry $doctrine
+  * @return void
+  */
+
+  public function show(Task $task, ManagerRegistry $doctrine)
+  {
+      return $this->render("tasks/show.html.twig", [
+            "task" => $task
+      ]);
+  }
+
+  /**
    * Edita uma tarefa no banco de dados
-   *
+   *  
+   * @Route("/{id}/edit", name="task_edit",  methods={"GET", "POST"})
    * @param Request $request
    * @param Task $task
    * @return Response
    */
   public function edit(Request $request, Task $task, ManagerRegistry $doctrine): Response
   {
-    if ($request->isMethod("POST")) {
+    $isTokenValid = $this->isCsrfTokenValid('cadastro_tarefas', $request->request->get('_token'));
+
+    if ($request->isMethod("POST") && $isTokenValid) {
         $task->setTitle($request->request->get('title'));
         $task->setDescription($request->request->get('description'));
 
@@ -103,16 +122,20 @@ class TaskController extends AbstractController
 
   /**
    * Apaga uma tarefa no banco de dados
-   *
+   * @Route("/{id}", name="task_delete",  methods="DELETE")
    * @param Task $task
    * @return Response
    */
   
-  public function delete(Task $task, ManagerRegistry $doctrine): Response
+  public function delete(Task $task, ManagerRegistry $doctrine, Request $request): Response
   {
-    $entity = $doctrine->getManager();
-    $entity->remove($task);
-    $entity->flush();
+    $isTokenValid = $this->isCsrfTokenValid('deletar_tarefa', $request->request->get('_token'));
+
+    if ($isTokenValid) {
+      $entity = $doctrine->getManager();
+      $entity->remove($task);
+      $entity->flush();
+    }
 
     return $this->redirectToRoute("task_index");
   }
