ab768a8fa326d3fa0eb480fb2597efae62a79a41
Monalisa Menezes <monalisa.menezes@onesight.com.br>
2022-09-18 12:05:15 -0300

    feat: Adiciona rota home e função de atualizar

--- src/Controller/ClienteController.php
+++ src/Controller/ClienteController.php
@@ -5,12 +5,21 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class ClienteController extends AbstractController
 {
     #[Route('/cliente', name: 'app_cliente')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        // var_dump(
+        //     $request->query->all()
+        // );
+
+        var_dump(
+            $request->get('cor')
+        );
+
         return $this->render('cliente/index.html.twig', [
             'controller_name' => 'ClienteController',
         ]);
--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -6,6 +6,7 @@ use App\Entity\Task;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
@@ -37,23 +38,80 @@ class TaskController extends AbstractController
   }
 
   /**
-   * Inserir uma tarefa
+   * Cria uma nova tarefa
    *
-   * @return void
+   * @param Request $request
+   * @return Response
    */
-  public function create(ManagerRegistry $doctrine)
+  public function new(ManagerRegistry $doctrine, Request $request) : Response
   {
-        $task = new Task;
-        $task->setTitle("Visitar o cliente X");
-        $task->setDescription("Visitar o cliente X por razão X");
-        $task->setScheduling(new \DateTime()); //hora atual
+        if ($request->isMethod("POST")) {
 
-        $entity = $doctrine->getManager();
-        $entity->persist($task);
-        $entity->flush();
+            // var_dump(
+            //   $request->request->get('title')
+            // );
 
-        $this->generateUrl("task_show", ["id" => $task->getId()]);
-        return $this->redirectToRoute("task_show", ["id" => $task->getId()]);
+            // var_dump($request->request->get("title") == ""); // verifica se o campo é vazio
+            // return new Response("");
+
+            $task = new Task;
+            $task->setTitle($request->request->get('title'));
+            $task->setDescription($request->request->get('description'));
+            $task->setScheduling(new \DateTime()); //hora atual
+
+            $entity = $doctrine->getManager();
+            $entity->persist($task);
+            $entity->flush();
+
+            $this->generateUrl("task_show", ["id" => $task->getId()]);
+            return $this->redirectToRoute("task_show", ["id" => $task->getId()]);
+        }
+
+        // var_dump($request->headers->all());
+
+        //var_dump($request->server->all());
+
+        // var_dump($request->attributes->all());
+        
+        return $this->render("tasks/new.html.twig");
+  }
+
+  /**
+   * Editamos as tarefas
+   *
+   * @param Request $request
+   * @param Task $task
+   * @return Response
+   */
+  public function edit(Request $request, Task $task, ManagerRegistry $doctrine): Response
+  {
+    if ($request->isMethod("POST")) {
+        $task->setTitle($request->request->get('title'));
+        $task->setDescription($request->request->get('description'));
+
+        $doctrine->getManager()->flush();
+
+        return $this->redirectToRoute("task_show", ['id' => $task->getId()]);
+    }
+    return $this->render("tasks/edit.html.twig", [
+      "task" => $task
+    ]);
+  }
+
+  /**
+   * Undocumented function
+   *
+   * @param Task $task
+   * @return Response
+   */
+  
+  public function delete(Task $task, ManagerRegistry $doctrine): Response
+  {
+    $entity = $doctrine->getManager();
+    $entity->remove($task);
+    $entity->flush();
+
+    return $this->redirectToRoute("task_index");
   }
 
 }   
